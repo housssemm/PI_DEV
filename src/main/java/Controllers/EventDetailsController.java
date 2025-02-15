f0eee574368ef7dd6db919d9871ff7b4119ff1f8
houssem <95879355+housssemm@users.noreply.github.com>
2025-02-15 19:04:32 +0100

    interface add event and get events version 5

--- src/main/java/Controllers/EventDetailsController.java
+++ src/main/java/Controllers/EventDetailsController.java
@@ -3,27 +3,55 @@ package Controllers;
 import Models.Evenement;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.ByteArrayInputStream;
 
 public class EventDetailsController {
 
+    @FXML private ImageView img;
     @FXML private Label titleLabel;
     @FXML private Label typeLabel;
     @FXML private Label descriptionLabel;
     @FXML private Label locationLabel;
     @FXML private Label priceLabel;
     @FXML private Label dateLabel;
+    @FXML private Label etatLabel;
+    @FXML private Label organisateurLabel;
+    @FXML private Label maxLabel;
 
     public void initialize() {
         // Empty initialize method, will be populated when event is passed.
     }
 
     public void setEventDetails(Evenement event) {
+
+            // Check if the event image is available
+            if (event.getImage() != null && event.getImage().length > 0) {
+                Image image = new Image(new ByteArrayInputStream(event.getImage()));
+                img.setImage(image);  // Set the image in the ImageView
+            } else {
+                // If there's no image, set a default image
+                img.setImage(new Image(getClass().getResourceAsStream("/path/to/default-image.jpg")));
+            }
         // Set the details of the event to the corresponding labels
+
         titleLabel.setText("Title: " + event.getTitre());
-        typeLabel.setText("Type: " + event.getType());
         descriptionLabel.setText("Description: " + event.getDescription());
         locationLabel.setText("Location: " + event.getLieu());
-        priceLabel.setText("Price: " + event.getPrix());
-        dateLabel.setText("Dates: " + event.getDateDebut() + " to " + event.getDateFin());
+        priceLabel.setText("prix: " + event.getPrix());
+        dateLabel.setText("Dates: " + event.getDateDebut() + " à " + event.getDateFin());
+        typeLabel.setText("Type: " + event.getType());
+        etatLabel.setText("Etat: " + event.getEtat());
+        organisateurLabel.setText("Organisateur: " + event.getOrganisateur());
+        maxLabel.setText("capacite Maximale : " + event.getCapaciteMaximale());
+
     }
 }
+
+
+
+
+
+
